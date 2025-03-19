1f01cf821fac32e6bb928b8a5b42a7b12665998f
Mik255 <mikael20informatica@gmail.com>
2025-03-19 19:13:47 -0300

    ajuets

--- app/main.py
+++ app/main.py
@@ -7,11 +7,14 @@ middleware = [
     Middleware(
         CORSMiddleware,
         allow_origins=["https://main.darhi3q3t4y0l.amplifyapp.com"],  # Domínio(s) específico(s)
+
         allow_methods=["*"],
         allow_headers=["*"]
     )
 ]
 app = FastAPI(middleware=middleware)
+# app.add_middleware(JWTAuthMiddleware, excluded_paths=["/", "/docs", "/openapi.json", "/sessions/login"])
+from app.recipes.controller import recipes_controller, session_controller,collection_controller, users_controller
 
 # Registrar o roteador do módulo Recipe
 app.include_router(recipes_controller.router, prefix="/recipes", tags=["Recipes"])
