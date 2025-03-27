7042ffae2622e919f2ef467429c7de85c8ed81d4
Mik255 <mikael20informatica@gmail.com>
2025-03-26 21:50:42 -0300

    ajues

--- app/recipes/repository/session_repository.py
+++ app/recipes/repository/session_repository.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from app.recipes.models.session import Session as SessionModel
 from app.recipes.models.recipe import Recipe
-from sqlalchemy.orm import Session, selectinload
+from sqlalchemy.orm import Session, selectinload,load_only
 def create_session(db: Session, model: SessionModel):
     db.add(model)
     db.commit()
@@ -12,12 +12,11 @@ def get_all_sessions(db: Session):
     return (
         db.query(SessionModel)
         .options(
-    selectinload(SessionModel.recipes)
-      .selectinload(Recipe.images)
-      .selectinload(Recipe.ingredients)
-      .selectinload(Recipe.preparations)
-      .selectinload(Recipe.categories)
-)
+            load_only(SessionModel.id, SessionModel.title, SessionModel.description, SessionModel.type, SessionModel.recipeType),
+            selectinload(SessionModel.recipes)
+                .load_only(Recipe.id, Recipe.title, Recipe.description, Recipe.property)
+        )
+        .all()
     )
 
 def add_recipe_to_session(db: Session, session_id: int, recipe_id: int):
